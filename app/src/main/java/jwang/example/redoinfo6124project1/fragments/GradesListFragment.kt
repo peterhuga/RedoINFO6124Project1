6f98dd24691f6fc14b86ba6282717e053598841c
Jianwei Wang <j_wang68715@fanshaweonline.ca>
2022-10-08 23:17:47 -0400

    Implemented view binding

--- app/src/main/java/jwang/example/redoinfo6124project1/fragments/GradesListFragment.kt
+++ app/src/main/java/jwang/example/redoinfo6124project1/fragments/GradesListFragment.kt
@@ -3,25 +3,30 @@ package jwang.example.redoinfo6124project1.fragments
 import android.os.Bundle
 import android.util.Log
 import android.view.*
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import androidx.appcompat.widget.Toolbar
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import jwang.example.redoinfo6124project1.GradeAdapter
+import jwang.example.redoinfo6124project1.MainActivity
 import jwang.example.redoinfo6124project1.R
+import jwang.example.redoinfo6124project1.databinding.FragmentGradesListBinding
 
 
 class GradesListFragment : Fragment() {
     private val toolbar: Toolbar by lazy {
         requireActivity().findViewById<Toolbar>(R.id.toolbar)
     }
-    private lateinit var recyclerView: RecyclerView
+    private lateinit var binding: FragmentGradesListBinding
+    //private lateinit var recyclerView: RecyclerView
     private lateinit var viewAdapter: GradeAdapter
     private lateinit var viewManager: RecyclerView.LayoutManager
 
-    private lateinit var fabLabLayout: ConstraintLayout
-    private lateinit var fabShowMenu: FloatingActionButton
+
+
 
     private var isFabShown = false
 
@@ -30,28 +35,22 @@ class GradesListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_grades_list, container, false)
-    }
+//        return inflater.inflate(R.layout.fragment_grades_list, container, false)
+        binding = FragmentGradesListBinding.inflate(inflater, container, false)
+        binding.fabShowMenu.setOnClickListener{
+            if (isFabShown) {
+                hideFabMenu()
+            } else {
+                showFabMenu()
+            }
+        }
+        binding.fabLayoutAddLab.visibility = ConstraintLayout.INVISIBLE
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        fabLabLayout = requireActivity().findViewById(R.id.fabLayoutAddLab)
-        fabShowMenu = requireActivity().findViewById(R.id.fabShowMenu)
-        fabLabLayout.visibility = ConstraintLayout.INVISIBLE
-
-
-//        setHasOptionsMenu(true)
-//        (context as MainActivity).supportActionBar?.setDisplayHomeAsUpEnabled(false)
-//        (context as MainActivity).supportActionBar?.setDisplayShowHomeEnabled(false)
-
-
-        //(context as MainActivity).scrollable_toolbar.isTitleEnabled = false
-        //(context as MainActivity).toolbar.title = "haha"
         toolbar.title = "My Grades"
-//        toolbar.setNavigationIcon(R.drawable.ic_arrow_back)
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back)
         toolbar.inflateMenu(R.menu.toolbar_main)
-//        toolbar.setNavigationOnClickListener{
-//        }
+        toolbar.setNavigationOnClickListener{
+        }
         toolbar.setOnMenuItemClickListener {
             when (it.itemId) {
                 R.id.item_about -> {
@@ -63,14 +62,18 @@ class GradesListFragment : Fragment() {
                 }
             }
         }
-        fabShowMenu.setOnClickListener{
-            if (isFabShown) {
-                hideFabMenu()
-            } else {
-                showFabMenu()
-            }
-        }
+        return binding.root
+
     }
+
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+//        setHasOptionsMenu(true)
+//        (context as MainActivity).supportActionBar?.setDisplayHomeAsUpEnabled(false)
+//        (context as MainActivity).supportActionBar?.setDisplayShowHomeEnabled(false)
+//        (context as MainActivity).scrollable_toolbar.isTitleEnabled = false
+//        (context as MainActivity).toolbar.title = "haha"
+//    }
 
 
 
@@ -82,13 +85,17 @@ class GradesListFragment : Fragment() {
 
     private fun showFabMenu() {
         isFabShown = true
-        fabLabLayout.visibility = ConstraintLayout.VISIBLE
+        binding.fabLayoutAddLab.visibility = ConstraintLayout.VISIBLE
     }
 
     private fun hideFabMenu() {
         isFabShown = false
-        fabLabLayout.visibility = ConstraintLayout.INVISIBLE
+        binding.fabLayoutAddLab.visibility = ConstraintLayout.INVISIBLE
 
+    }
+
+    private fun fabAnimation(view:View) {
+        //val anim: Animation = AnimationUtils.loadAnimation(view.context, translate_position )
     }
 
     companion object {

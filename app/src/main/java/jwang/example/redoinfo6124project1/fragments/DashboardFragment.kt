a241640dc120f9ceb1c8f0240441164fa6e0c413
Jianwei Wang <j_wang68715@fanshaweonline.ca>
2022-10-09 23:48:52 -0400

    Fixed toolbar duplicate issue. Complete card view design.

--- app/src/main/java/jwang/example/redoinfo6124project1/fragments/DashboardFragment.kt
+++ app/src/main/java/jwang/example/redoinfo6124project1/fragments/DashboardFragment.kt
@@ -1,12 +1,11 @@
 package jwang.example.redoinfo6124project1.fragments
 
+import android.app.SearchManager
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.provider.AlarmClock.EXTRA_MESSAGE
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.AdapterView
 import android.widget.AdapterView.OnItemClickListener
 import androidx.appcompat.widget.Toolbar
@@ -19,6 +18,7 @@ import jwang.example.redoinfo6124project1.models.Course
 
 
 const val ARG_PARAM1 = "param1"
+const val ARG_PARAM2 = "param2"
 
 
 
@@ -31,12 +31,12 @@ class DashboardFragment : Fragment() {
     private lateinit var binding: FragmentDashboardBinding
 
     val courses: List<Course> = listOf(
-        Course("INFO6124", "Android Application Development", 0),
-        Course("INFO6125", "iOS Application Development", 0),
+        Course("INFO6124", "Android App Development", 0),
+        Course("INFO6125", "iOS App Development", 0),
         Course("INFO6126", "UI/UX Design" , 0),
         Course("INFO6127", "Enterprise Platform" , 0),
         Course("INFO6128", "Progressive Web Application" , 0),
-        Course("INFO6129", "Cross-platform Application Development" , 0)
+        Course("INFO6129", "Cross-platform App Development" , 0)
     )
 
 
@@ -52,35 +52,55 @@ class DashboardFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentDashboardBinding.inflate(inflater, container, false)
-        toolbar.title = "Redo 6124 Project1"
+        //toolbar.title = "Redo 6124 Project1"
+        setHasOptionsMenu(true)
 
-        toolbar.inflateMenu(R.menu.toolbar_main)
 
-        toolbar.setOnMenuItemClickListener {
-            when (it.itemId) {
-                R.id.item_about -> {
-                    Log.d("MyTag", "about clicked")
-                    true
-                }
-                else -> {
-                    super.onOptionsItemSelected(it)
-                }
-            }
-        }
+  //      toolbar.inflateMenu(R.menu.toolbar_main)
+//
+//        toolbar.setOnMenuItemClickListener {
+//            when (it.itemId) {
+//                R.id.item_about -> {
+//                    Log.d("MyTag", "about clicked")
+//                    true
+//                }
+//                else -> {
+//                    super.onOptionsItemSelected(it)
+//                }
+//            }
+//        }
 
         binding.listViewCourses.adapter = CourseAdapter(requireContext(), courses)
         binding.listViewCourses.onItemClickListener =
             OnItemClickListener { parent, view, position, id ->
-                val fragParam = courses.get(position).courseCode
+                val courseCode = courses.get(position).courseCode
+                val courseName = courses.get(position).courseName
                 val transaction = fragmentManager?.beginTransaction()
                 if (transaction != null) {
                     transaction.replace(
                         R.id.fragment_placeholder,
-                        GradesListFragment.newInstance(fragParam)
+                        GradesListFragment.newInstance(courseCode, courseName)
                     ).addToBackStack(null).commit()
                 }
             }
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        (context as MainActivity).supportActionBar?.setDisplayHomeAsUpEnabled(false)
+        (context as MainActivity).supportActionBar?.setDisplayShowHomeEnabled(false)
+
+        //(context as MainActivity).scrollable_toolbar.isTitleEnabled = false
+        (context as MainActivity).toolbar.title = getString(R.string.app_name)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+
+        inflater.inflate(R.menu.toolbar_main, menu)
+
+        super.onCreateOptionsMenu(menu, inflater)
     }
 
     companion object {
@@ -89,6 +109,7 @@ class DashboardFragment : Fragment() {
             DashboardFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
+
                 }
             }
     }
--- app/src/main/java/jwang/example/redoinfo6124project1/fragments/GradesListFragment.kt
+++ app/src/main/java/jwang/example/redoinfo6124project1/fragments/GradesListFragment.kt
@@ -11,12 +11,14 @@ import androidx.core.view.ViewCompat.animate
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import jwang.example.redoinfo6124project1.GradeAdapter
+import jwang.example.redoinfo6124project1.MainActivity
 import jwang.example.redoinfo6124project1.R
 import jwang.example.redoinfo6124project1.databinding.FragmentGradesListBinding
 
 
 class GradesListFragment : Fragment() {
     private var param1: String? = null
+    private var param2: String? = null
     private val toolbar: Toolbar by lazy {
         requireActivity().findViewById<Toolbar>(R.id.toolbar)
     }
@@ -34,6 +36,7 @@ class GradesListFragment : Fragment() {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
         }
     }
 
@@ -51,41 +54,29 @@ class GradesListFragment : Fragment() {
             }
         }
         binding.fabLayoutAddLab.visibility = ConstraintLayout.INVISIBLE
+        binding.fabLayoutAddExam.visibility = ConstraintLayout.INVISIBLE
 
-        toolbar.title = param1
-        toolbar.setNavigationIcon(R.drawable.ic_arrow_back)
-        toolbar.inflateMenu(R.menu.toolbar_main)
-        toolbar.setNavigationOnClickListener{
-            fragmentManager?.popBackStack()
-        }
-        toolbar.setOnMenuItemClickListener {
-            when (it.itemId) {
-                R.id.item_about -> {
-                    Log.d("MyTag", "about clicked")
-                    true
-                }
-                else -> {
-                    super.onOptionsItemSelected(it)
-                }
-            }
-        }
+
+        binding.tvCourseName.text = param2
+        setHasOptionsMenu(true)
         return binding.root
 
     }
 
-//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        super.onViewCreated(view, savedInstanceState)
-//        setHasOptionsMenu(true)
-//        (context as MainActivity).supportActionBar?.setDisplayHomeAsUpEnabled(false)
-//        (context as MainActivity).supportActionBar?.setDisplayShowHomeEnabled(false)
-//        (context as MainActivity).scrollable_toolbar.isTitleEnabled = false
-//        (context as MainActivity).toolbar.title = "haha"
-//    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //(context as MainActivity).supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_arrow_back)
+        (context as MainActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
+        (context as MainActivity).toolbar.title = param1
+    }
 
     private fun showFabMenu() {
         isFabShown = true
         //binding.fabShowMenu.isClickable = false
         binding.fabLayoutAddLab.visibility = ConstraintLayout.VISIBLE
+        binding.fabLayoutAddExam.visibility = ConstraintLayout.VISIBLE
 
         binding.fabLayoutAddLab.animate().translationYBy(
             -resources.getDimension(R.dimen.standard_155) - 60).apply {
@@ -95,6 +86,20 @@ class GradesListFragment : Fragment() {
                 duration = 75
             }
         }
+
+        binding.fabLayoutAddExam.animate().translationYBy(
+            -resources.getDimension(R.dimen.standard_105) - 40).apply {
+            duration = 100
+        }.withEndAction{
+            binding.fabLayoutAddExam.animate().translationYBy(40.toFloat()).apply {
+                duration = 75
+            }
+        }
+
+
+
+
+
         binding.fabShowMenu.animate().duration = 100
         binding.fabShowMenu.animate().rotationBy(45.0f)
 
@@ -106,15 +111,39 @@ class GradesListFragment : Fragment() {
         binding.fabLayoutAddLab.animate()
             .translationYBy(resources.getDimension(R.dimen.standard_155))
             .withEndAction { binding.fabLayoutAddLab.visibility = ConstraintLayout.INVISIBLE }
+        binding.fabLayoutAddExam.animate()
+            .translationYBy(resources.getDimension(R.dimen.standard_105))
+            .withEndAction { binding.fabLayoutAddExam.visibility = ConstraintLayout.INVISIBLE }
         binding.fabShowMenu.animate().rotationBy(-45.0f)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+
+        inflater.inflate(R.menu.toolbar_main, menu)
+
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item?.itemId) {
+            android.R.id.home -> {
+                (context as MainActivity).supportFragmentManager.popBackStackImmediate()
+            }
+            R.id.item_about -> {
+                    Log.d("MyTag", "about clicked")
+                    true
+                }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     companion object {
         @JvmStatic
-        fun newInstance(param1: String) =
+        fun newInstance(param1: String, param2: String) =
             GradesListFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
                 }
             }
     }
